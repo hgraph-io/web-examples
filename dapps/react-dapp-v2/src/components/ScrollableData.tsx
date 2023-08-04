00cf4c7952626894c185a7ba2b3df0d581723f82
Fenton Taylor <fentontaylor@gmail.com>
2023-08-04 13:53:49 -0600

    [dapp-v2] Add styling for data scrollbars

--- dapps/react-dapp-v2/src/components/ScrollableData.tsx
+++ dapps/react-dapp-v2/src/components/ScrollableData.tsx
@@ -7,6 +7,25 @@ const SScrollableArea = styled.div`
   background: rgba(${colors.lightGrey}, 0.5);
   padding: 8px;
   border-radius: 4px;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+    height: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: rgb(212, 212, 212);
+    border-radius: 3px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: rgba(88, 91, 99, 0.7);
+    border-radius: 3px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: rgb(88, 91, 99);
+  }
 `;
 
 const PreformattedData = styled.pre`
